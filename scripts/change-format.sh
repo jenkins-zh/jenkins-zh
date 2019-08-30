8b5021fcc91ba275e15ebb8af4829041c4e6543b
Zhao Xiaojie <LinuxSuRen@gmail.com>
2019-08-30 17:35:57 +0800

    Fix the logic of format
    do not need to copy images

--- scripts/change-format.sh
+++ scripts/change-format.sh
@@ -7,4 +7,3 @@ rm -rf ${root_dir}/content/wechat/articles/2018
 rm -rf ${root_dir}/content/wechat/images/*.md
 rm -rf ${root_dir}/content/wechat/management/
 rm -rf ${root_dir}/content/wechat/*.md
-cp -r ${root_dir}/content/wechat/images/ static/images/
